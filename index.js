ddee9a83da843ab6ef107dce525ee82079d18915
Tamar baboghlian <tamara.baboghlian@gmail.com>
2020-03-22 20:47:41 +0400

    code cleanup

--- index.js
+++ index.js
@@ -28,14 +28,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 //routes
 app.get('/', (req, res) => {
-    console.log(`Get request`);
     res.render('index');
 })
 
 app.post('/publish', (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    console.log(`POST request is ${(req.body.message)}`);
     io.emit('chat message', req.body.message);
     res.sendStatus(200);
 })
@@ -44,20 +42,16 @@ app.post('/publish', (req, res) => {
 app.use(error)
 
 io.on('connection', (socket) => {
-    console.log('client connected');
     socket.on('chat message', function (data) {
-        console.log('message: ' + data.message);
         io.emit('chat message', data.message);
     });
     socket.on('disconnect', function () {
-        console.log('client disconnected');
     });
 });
 
 const port = process.env.PORT || 3000;
 
 http.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
 });
 
 function validate(req) {
